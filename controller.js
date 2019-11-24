e6687819f6fda9e97cb5a75a6884350dfa35d836
Rastikko <rastikko@gmail.com>
2019-11-24 14:22:23 -0500

    try amd module

--- controller.js
+++
@@ -1,5 +0,0 @@
-function CreateCharacter(args) {
-    log.info("hello World!");
-    return true;
-}
-export default { CreateCharacter };
--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-import Controller from 'controller';
-handlers["CreateCharacter"] = Controller.CreateCharacter;
+define("controller", ["require", "exports"], function (require, exports) {
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function CreateCharacter(args) {
+        log.info("hello World!");
+        return true;
+    }
+    exports.default = { CreateCharacter };
+});
+define("main", ["require", "exports", "controller"], function (require, exports, controller_1) {
+    Object.defineProperty(exports, "__esModule", { value: true });
+    handlers["CreateCharacter"] = controller_1.default.CreateCharacter;
+});
