6380bc4eaffd265004736dd93f6e4e60666d8df9
Rastikko <rastikko@gmail.com>
2019-11-24 14:30:08 -0500

    test including require.js library

---
+++ controller.js
@@ -0,0 +1,6 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+function CreateCharacter(args) {
+    log.info("hello World!");
+    return true;
+}
+exports.default = { CreateCharacter };
--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-define("controller", ["require", "exports"], function (require, exports) {
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function CreateCharacter(args) {
-        log.info("hello World!");
-        return true;
-    }
-    exports.default = { CreateCharacter };
-});
-define("main", ["require", "exports", "controller"], function (require, exports, controller_1) {
-    Object.defineProperty(exports, "__esModule", { value: true });
-    handlers["CreateCharacter"] = controller_1.default.CreateCharacter;
-});
+Object.defineProperty(exports, "__esModule", { value: true });
+const controller_1 = require("controller");
+handlers["CreateCharacter"] = controller_1.default.CreateCharacter;
