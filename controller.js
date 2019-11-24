6cda0949b55d8cec145d4b41640fb5dcf7adb273
Rastikko <rastikko@gmail.com>
2019-11-24 14:40:45 -0500

    use amd instead of common

--- controller.js
+++
@@ -1,6 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-function CreateCharacter(args) {
-    log.info("hello World!");
-    return true;
-}
-exports.default = { CreateCharacter };
