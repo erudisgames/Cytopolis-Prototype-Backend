e54bd3e9a98b84e4eab8efb4396ab54e70c3db66
Rastikko <rastikko@gmail.com>
2019-11-29 08:46:48 -0500

    initialize data with empty arrays

--- src/services/title-data-service.ts
+++ src/services/title-data-service.ts
@@ -1,7 +1,7 @@
 class TitleDataService
 {
-    public generators : GeneratorTitleData[];
-    public enzymes : EnzymeTitleData[];
+    public generators : GeneratorTitleData[] = [];
+    public enzymes : EnzymeTitleData[] = [];
 
     public FetchData() : void
     {
@@ -9,14 +9,14 @@ class TitleDataService
         const titleDataRequest = { "Keys": ["Enzymes", "Generators"] };
         const titleDataResult = server.GetTitleData(titleDataRequest);
 
-        if (titleDataResult.Data.hasOwnProperty("Generators"))
-        {
-            this.generators = JSON.parse(titleDataResult.Data["Generators"]);
-        }
-
         if (titleDataResult.Data.hasOwnProperty("Enzymes"))
         {
             this.enzymes = JSON.parse(titleDataResult.Data["Enzymes"]);
+        }
+
+        if (titleDataResult.Data.hasOwnProperty("Generators"))
+        {
+            this.generators = JSON.parse(titleDataResult.Data["Generators"]);
         }
     }
 }
